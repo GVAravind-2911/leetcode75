171f059bd87543b8dca3ba24aa97fadf0f8930e5
GVAravindDSB <gvaravinddsb@gmail.com>
2025-03-04 14:27:22 +0530

    add KidsWithCandies class with method to determine which kids can have the greatest number of candies

---
+++ src/KidsWithCandies1431.java
@@ -0,0 +1,21 @@
+import java.util.*;
+
+class KidsWithCanies1431 {
+    public List<Boolean> kidsWithCandies(int[] candies, int extraCandies) {
+        List<Integer> arr = new ArrayList<>();
+        int largest = candies[0];
+        for (int candy : candies) {
+            arr.add(candy);
+            if (candy>largest){
+                largest = candy;
+            }
+        }
+
+        List<Boolean> outp = new ArrayList<>();
+        for (int i : arr) {
+            outp.add((i + extraCandies) >= largest);
+        }
+
+        return outp;
+    }
+}
